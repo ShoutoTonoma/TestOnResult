c035065f07cf24fc8cd1f7ceba9ce5b15d9b7c7c
shoutotonoma <shoutotonomanvidianow@gmail.com>
2022-07-13 14:52:46 +0500

    mytestapp

---
+++ app/src/main/java/com/example/testonresult/MainActivity2.kt
@@ -0,0 +1,20 @@
+package com.example.testonresult
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+
+    fun onClick(view: View){
+        val i = Intent()
+        i.putExtra("key1", "done")
+        setResult(RESULT_OK, i)
+        finish()
+    }
+}
